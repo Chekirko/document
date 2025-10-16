a3bc785d0e616189d73e325c23c9354003c60d6f
chekirko <victorche59@gmail.com>
2025-10-16 09:55:02 +0300

    fix clerk

---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,11 @@
+// app/not-found.tsx
+"use client";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
